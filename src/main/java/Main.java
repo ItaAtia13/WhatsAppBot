5dcb45b53c7a3aa0fdd006c12e9eeab70e39f09b
ItamarAtia <atia49@gmail.com>
2022-06-03 15:08:35 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+
+public class Main extends JFrame {
+    public static void main(String[] args) throws ParseException {
+        //frame
+        JFrame frame = new JFrame("Whatsapp-Bot");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+
+
+        JButton btn=new JButton("click");
+
+
+        // TODO: add placeholders
+        frame.add(new TextField("Enter your Phone number"));
+        frame.add(new TextField("Enter a Message"));
+        frame.add(btn);
+        frame.setVisible(true);
+
+        //selenium
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\atia4\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == btn) {
+                    ChromeDriver driver = new ChromeDriver();
+                   driver.get("https://web.whatsapp.com/");
+                    driver.manage().window().maximize();
+                    while  (true){
+                    if (driver.findElements(By.className("_3ArsE")).size()==1) {
+                        System.out.println("Login successfully");
+                        JLabel successLabel=new JLabel("Login successfully");
+                        frame.add(successLabel);
+                        frame.setVisible(true);
+
+                        break;
+
+                    }
+                    }
+
+
+                }
+            }
+        });
+
+
+
+//        if (topArticle!=null) {
+//            topArticle.click();
+//        }
+//        WebElement searchTextField=driver.findElement(By.id("one-search"));
+//        if (searchTextField!=null){
+//            searchTextField.sendKeys("Shai Givati");
+//        }
+
+    }
+}
