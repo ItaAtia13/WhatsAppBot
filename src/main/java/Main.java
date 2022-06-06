6761bcaacdbb82c125411e8747dc24e8e44ff901
ItamarAtia <atia49@gmail.com>
2022-06-06 20:09:39 +0300

    commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,71 +2,135 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.awt.FlowLayout;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.text.ParseException;
+import java.time.Duration;
+
+import static java.lang.Character.isDigit;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws ParseException {
-        //frame
-        JFrame frame = new JFrame("Whatsapp-Bot");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+    public Main() {
+        //Panel p=new Panel(new FlowLayout(FlowLayout.LEADING));
+        this.setTitle("Whatsapp-Bot");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 400);
 
-        frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JButton btn = new JButton("Click here for login to your WhatsApp number");
+        String specificUrl="https://api.whatsapp.com/send?phone=972";
+          String whatsapp="https://web.whatsapp.com/";
 
 
+        this.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 10));
 
-        JButton btn=new JButton("click");
-        JLabel phoneLabel=new JLabel("Phone number:");
-        JLabel messageLabel=new JLabel("Enter a Message:");
+        JLabel phoneLabel = new JLabel("Phone number:");
+        TextField phoneField = new TextField(10);
+
+        // phoneLabel.setBounds(50,100,200,200);
+        JLabel messageLabel = new JLabel("Enter a Message:");
+        TextField messageField = new TextField(10);
+        // messageLabel.setBounds(50,150,200,200);
 
 
         // TODO: add placeholders
-        frame.add(phoneLabel);
-        frame.add(new TextField(10));
-        frame.add(messageLabel);
-        frame.add(new TextField(10));
-        frame.add(btn);
-        frame.setVisible(true);
+        this.add(phoneLabel);
+        this.add(phoneField);
+        this.add(messageLabel);
+        this.add(messageField);
+        this.add(btn);
+        this.setVisible(true);
 
         //selenium
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\atia4\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
 
+
+
+
+
         btn.addActionListener(e -> {
-            if(e.getSource() == btn) {
-                ChromeDriver driver = new ChromeDriver();
-               driver.get("https://web.whatsapp.com/");
+            if (phoneField.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Phone field is empty ",
+                        "Hey!", JOptionPane.ERROR_MESSAGE);
+            } else if (!correctPhoneNumber(phoneField.getText())) {
+                JOptionPane.showMessageDialog(null, "Invalid Phone number ",
+                        "Hey!", JOptionPane.ERROR_MESSAGE);
+            } else if (messageField.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "massage field is empty ",
+                        "Hey!", JOptionPane.ERROR_MESSAGE);
+            } else if (e.getSource() == btn) {
+
+                // configurations
+                ChromeOptions chromeOptions=new ChromeOptions();
+                chromeOptions.addArguments("user-data-dir=C:\\Users\\atia4\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
+                ChromeDriver driver = new ChromeDriver(chromeOptions);
+
+                //start private conversation with the number that was entered by the user
+                driver.get(specificUrl+phoneField.getText().substring(1));
+//                driver.get(whatsapp);
+
                 driver.manage().window().maximize();
-                while  (true){
-                if (driver.findElements(By.className("_3ArsE")).size()==1) {
-                    System.out.println("Login successfully");
-                    JLabel successLabel=new JLabel("Login successfully");
-                    frame.add(successLabel);
-                    frame.setVisible(true);
 
-                    break;
+                while (true) {
+//                    System.out.println("gfdg");
+                    if (driver.findElements(By.className("_3ArsE")).size() == 1) {
+                        System.out.println("Login successfully");
+                        JOptionPane.showMessageDialog(null,"Login successfully");
+                        JLabel successLabel = new JLabel("Login successfully");
+                        this.add(successLabel);
+                        this.setVisible(true);
+
+//try
+//                WebElement textField=driver.findElement(By.cssSelector("input[role=\"textbox\"]"));
+//                        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(8));
+//                        WebElement textField= wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("_13NKt copyable-text selectable-text")));
+                        WebElement textField=driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]"));
+                        System.out.println("reader");
+                        textField.sendKeys("ddd");
+                        break;
+
+                    }
 
                 }
-                }
+
 
 
             }
         });
 
+    }
+
+    private boolean correctPhoneNumber(String phoneNumber) {
+        boolean correct=false;
+        boolean onlyNumbers = false;
+        for (int j = 0; j < phoneNumber.length(); j++) {
+            if (isDigit(phoneNumber.charAt(j))) {
+                onlyNumbers = true;
+            }
+        }
+
+        if (onlyNumbers) {
+            if(phoneNumber.startsWith("05")&&phoneNumber.length()==10){
+                correct=true;
+            }
+        }
+        return correct;
+    }
+
+    public static void main(String[] args) throws ParseException {
+        new Main();
 
 
 //        if (topArticle!=null) {
 //            topArticle.click();
 //        }
-//        WebElement searchTextField=driver.findElement(By.id("one-search"));
 //        if (searchTextField!=null){
 //            searchTextField.sendKeys("Shai Givati");
 //        }
